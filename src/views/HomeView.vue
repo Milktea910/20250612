12e7a7cd7a3aa44fc296bf11eb7714adb7e37455
114FrontEnd <milkteaop12345@gmail.com>
2025-06-12 13:57:33 +0800

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,97 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols="12">
+        <h1>目前事項</h1>
+        <h2>{{ lists.currentItem }}</h2>
+        <DigitNumber v-for="(data, i) in timeLeftText" :key="i" color="white" :data="data" />
+      </v-col>
+      <v-col cols="12">
+        <v-btn
+          :disabled="
+            status === STATUS.COUNTING ||
+            (lists.currentItem.length === 0 && lists.items.length === 0)
+          "
+          icon="mdi-play"
+          @click="startTimer()"
+        />
+        <v-btn :disabled="status !== STATUS.COUNTING" icon="mdi-pause" @click="pause" />
+        <v-btn :disabled="lists.currentItem.length === 0" icon="mdi-skip-next" @click="finish" />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script setup>
+import { useWebNotification } from '@vueuse/core'
+import { computed, ref } from 'vue'
+import DigitNumber from '@/components/DigitNumber.vue'
+import { useListStore } from '@/stores/list'
+import { useSettingsStore } from '@/stores/setting'
+
+const lists = useListStore()
+const setting = useSettingsStore()
+
+const STATUS = {
+  STOP: 0,
+  COUNTING: 1,
+  PAUSE: 2,
+}
+const status = ref(STATUS.STOP)
+
+let timer = 0
+
+const startTimer = () => {
+  if (status.value === STATUS.STOP && lists.items.length > 0) {
+    lists.setCurrentItem()
+  }
+
+  status.value = STATUS.COUNTING
+
+  timer = setInterval(() => {
+    lists.countdown()
+
+    if (lists.timeleft <= 0) {
+      finish(timer)
+    }
+  }, 1000)
+}
+
+const pause = () => {
+  clearInterval(timer)
+  status.value = STATUS.PAUSE
+}
+
+const finish = () => {
+  clearInterval(timer)
+
+  status.value = STATUS.STOP
+
+  const audio = new Audio()
+  audio.src = setting.selectedAlarm.file
+  audio.play()
+
+  const { show, isSupported } = useWebNotification({
+    title: '事項完成',
+    body: lists.currentItem,
+    icon: new URL('@/assets/tomato.png', import.meta.url).href,
+  })
+  if (isSupported.value) {
+    show()
+  }
+
+  lists.setFinishItem()
+
+  if (lists.items.length > 0) {
+    startTimer()
+  }
+}
+
+const timeLeftText = computed(() => {
+  const m = Math.floor(lists.timeleft / 60)
+    .toString()
+    .padStart(2, '0')
+  const s = (lists.timeleft % 60).toString().padStart(2, '0')
+  return m + ':' + s
+})
+</script>
---
+++ src/views/ListView.vue
@@ -0,0 +1,118 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols="12">
+        <h1 class="text-center">未完成</h1>
+      </v-col>
+      <v-col cols="12">
+        <!-- <v-form @submit.prevent="onAddFormSubmit"> -->
+        <v-text-field
+          ref="inputTextField"
+          v-model="input"
+          append-icon="mdi-plus"
+          clearable
+          label="新增事項"
+          :rules="[rules.length, rules.required]"
+          @click:append="onAddFormSubmit"
+          @keydown.enter="onAddFormSubmit"
+        />
+        <!-- </v-form> -->
+        <v-table>
+          <thead>
+            <tr>
+              <th>事項</th>
+              <th>操作</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-if="lists.items.length === 0">
+              <td colspan="2">沒有項目</td>
+            </tr>
+            <tr v-for="(list, i) in lists.items" :key="list.id">
+              <td>
+                <v-text-field
+                  v-show="list.edit"
+                  ref="editTextField"
+                  v-model="list.model"
+                  autofocus
+                  :rules="[rules.required, rules.length]"
+                />
+                <span v-show="!list.edit">{{ list.text }}</span>
+              </td>
+              <td>
+                <template v-if="list.edit">
+                  <v-btn icon="mdi-check" @click="onEditSubmit(list.id, i)" />
+                  <v-btn icon="mdi-undo" @click="lists.cancelEdit(list.id)" />
+                </template>
+                <template v-else>
+                  <v-btn icon="mdi-pencil" @click="lists.editItem(list.id)" />
+                  <v-btn icon="mdi-delete" @click="lists.delItem(list.id)" />
+                </template>
+              </td>
+            </tr>
+          </tbody>
+        </v-table>
+      </v-col>
+      <v-col cols="12">
+        <h1 class="text-center">已完成</h1>
+        <v-table>
+          <thead>
+            <tr>
+              <th>事項</th>
+              <th>操作</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-if="lists.finishedItems.length === 0">
+              <td colspan="2">沒有項目</td>
+            </tr>
+            <tr v-for="list in lists.finishedItems" :key="list.id">
+              <td>{{ list.text }}</td>
+              <td>
+                <v-btn icon="mdi-delete" @click="lists.delFinishedItem(list.id)" />
+              </td>
+            </tr>
+          </tbody>
+        </v-table>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script setup>
+import { ref, useTemplateRef } from 'vue'
+import { useListStore } from '@/stores/list'
+
+const lists = useListStore()
+const input = ref('')
+// const inputTextField = ref(null)
+const inputTextField = useTemplateRef('inputTextField')
+const editTextField = useTemplateRef('editTextField')
+
+// const onAddFormSubmit = async(event) => {
+//   const result = await event
+//   if(!result.valid) return
+//   lists.addItem(input.value)
+//   input.value = ''
+// }
+
+const onAddFormSubmit = () => {
+  if (!inputTextField.value.isValid) return
+  lists.addItem(input.value)
+  inputTextField.value.reset()
+}
+const onEditSubmit = (id, i) => {
+  if (!editTextField.value[i].isValid) return
+  lists.submitEdit(id)
+}
+
+const rules = {
+  required: value => {
+    // return Boolean(value)||'必填'
+    return !!value || '必填'
+  },
+  length: value => {
+    return value.length >= 3 || '必須3個字以上'
+  },
+}
+</script>
